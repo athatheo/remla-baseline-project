05959b3b286a9259d7419e66a04f4a1404ab41e6
athatheo <athatheoc@gmail.com>
2022-06-02 17:01:16 +0200

    Added automatic data collection

---
+++ src/data_collection.py
@@ -0,0 +1,20 @@
+from stackapi import StackAPI
+from text_preprocessing import read_data
+
+SITE = StackAPI('stackoverflow')
+questions = SITE.fetch('questions')
+
+data = read_data("../data/raw/train/train.tsv")
+tag_counts = {}
+for tags in data['tags']:
+    for tag in tags:
+        if tag in tag_counts:
+            tag_counts[tag] += 1
+        else:
+            tag_counts[tag] = 1
+max_keyword = max(tag_counts, key=tag_counts.get)
+file_object = open('../data/raw/train/train.tsv', 'a')
+for item in questions['items']:
+    if max_keyword not in item['tags']:
+        file_object.write(item['title'] + '\t' + item['tags'] + '\n')
+
